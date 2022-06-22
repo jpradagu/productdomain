8369ad862ce56412dc3818c5f7e578df14c9d8c0
Jimmy Prada <jipragu@emeal.nttdata.com>
2022-06-22 15:40:49 -0500

    fix checkstyle

--- src/test/java/com/nttdata/bootcamp/productdomain/ProductdomainApplicationTests.java
+++ src/test/java/com/nttdata/bootcamp/productdomain/ProductdomainApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductdomainApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
